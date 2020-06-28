9d2098051ff851a840a4af6737b36f825d750146
Will <2623556566@qq.com>
2020-06-28 18:43:46 +0800

    asd

--- Project/CARLA_0.9.9.4/WindowsNoEditor/PythonAPI/rl/reinforcement_learning_control.py
+++ Project/CARLA_0.9.9.4/WindowsNoEditor/PythonAPI/rl/reinforcement_learning_control.py
@@ -65,120 +65,158 @@ class CarEnv:
         self.world = self.client.get_world()
         self.blueprint_library = self.world.get_blueprint_library()
         self.model_3 = self.blueprint_library.filter("model3")[0]
-        self.image_sensor_input = None
+        self.sem_camera_input = None
+        self.depth_camera_input = None
+        self.actor_list = []
+        self.collision_hist = []
+        self.lane_invasion = []
+        self.acceleration = None
+        self.angular_velocity = None
+
+        random.seed(42)
+        np.random.seed(42)
+        tf.random.set_seed(42)
+
+    def __del__(self):
+        self.clear_env()
+
+    def clear_env(self):
+        for actor in env.actor_list:
+            actor.destroy()
+        self.sem_camera_input = None
+        self.depth_camera_input = None
+        self.actor_list = []
+        self.collision_hist = []
+        self.lane_invasion = []
+        self.acceleration = None
+        self.angular_velocity = None
+
+    def init_vehicle(self):
+        vehicle_transform = None
+        # 解决出现在障碍物处问题
+        loop = True
+        while loop:
+            loop = False
+            try:
+                vehicle_transform = random.choice(self.world.get_map().get_spawn_points())
+                self.vehicle = self.world.spawn_actor(self.model_3, vehicle_transform)
+            except RuntimeError:
+                loop = True
+
+        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.0))
+        self.actor_list.append(self.vehicle)
+        return vehicle_transform
 
     def rgb_camera_callback(self, image):
         # RGBA
         img = np.array(image.raw_data).reshape((self.img_height, self.img_width, 4))
         img = img[:, :, :3]
-        if self.show_camera:
-            cv2.namedWindow('rgb_camera', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow("rgb_camera", img)
-            cv2.waitKey(15)
+        cv2.namedWindow('rgb_camera', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow("rgb_camera", img)
+        cv2.waitKey(15)
 
     def sem_camera_callback(self, image):
+        image.convert(carla.ColorConverter.CityScapesPalette)
         img = np.array(image.raw_data).reshape((self.img_height, self.img_width, 4))
         img = img[:, :, :3]
         if self.show_camera:
             cv2.namedWindow('semantic_segmentation', cv2.WINDOW_AUTOSIZE)
             cv2.imshow("semantic_segmentation", img)
             cv2.waitKey(15)
-        self.image_sensor_input = img
+        self.sem_camera_input = img
 
-    def collision_data(self, event):
+    def depth_camera_callback(self, image):
+        # image.convert(carla.ColorConverter.Depth)
+        image.convert(carla.ColorConverter.LogarithmicDepth)
+        img = np.array(image.raw_data).reshape((self.img_height, self.img_width, 4))
+        img = img[:, :, :3]
+        if self.show_camera:
+            cv2.namedWindow('depth_camera', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow("depth_camera", img)
+            cv2.waitKey(15)
+        self.depth_camera_input = img
+
+    def collision_callback(self, event):
         self.collision_hist.append(event)
 
+    def lane_callback(self, lane):
+        self.lane_invasion = lane.crossed_lane_markings
+
+    def imu_callback(self, imu):
+        self.acceleration = imu.accelerometer
+        self.angular_velocity = imu.gyroscope
+
     def reset(self):
-        self.collision_hist = []
-        self.actor_list = []
+        self.clear_env()
 
-        random.seed(42)
-        np.random.seed(42)
-        tf.random.set_seed(42)
+        vehicle_transform = self.init_vehicle()
+        camera_transform = carla.Transform(carla.Location(x=2, y=0, z=1), carla.Rotation(0, 180, 0))
+        other_transform = carla.Transform(carla.Location(0, 0, 0), carla.Rotation(0, 0, 0))
 
-        # 解决出现在障碍物处问题
-        loop = True
-        while loop:
-            loop = False
-            try:
-                self.transform = random.choice(self.world.get_map().get_spawn_points())
-                self.vehicle = self.world.spawn_actor(self.model_3, self.transform)
-            except RuntimeError:
-                # print("spawn_actor at collision.")
-                loop = True
-
-        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.0))
-        self.actor_list.append(self.vehicle)
-
-        """
-        深度相机、LIDAR光线投射传感器
-        """
-        # 初始化RGB相机
-        rgb_camera = self.blueprint_library.find("sensor.camera.rgb")
-        rgb_camera.set_attribute("image_size_x", f"{self.img_width}")
-        rgb_camera.set_attribute("image_size_y", f"{self.img_height}")
-        rgb_camera.set_attribute("fov", "110")
-
-        transform = carla.Transform(carla.Location(x=2, y=0, z=1))
-        rgb_camera = self.world.spawn_actor(rgb_camera, transform, attach_to=self.vehicle,
-                                            attachment_type=carla.AttachmentType.SpringArm)
-        rgb_camera.listen(lambda data: self.rgb_camera_callback(data))
-        self.actor_list.append(rgb_camera)
-
-        # 初始化语义分割相机
+        # 语义分割相机
         sem_camera = self.blueprint_library.find("sensor.camera.semantic_segmentation")
         sem_camera.set_attribute("image_size_x", f"{self.img_width}")
         sem_camera.set_attribute("image_size_y", f"{self.img_height}")
         sem_camera.set_attribute("fov", "110")
 
-        transform = carla.Transform(carla.Location(x=2, y=0, z=1))
-        sem_camera = self.world.spawn_actor(sem_camera, transform, attach_to=self.vehicle,
+        sem_camera = self.world.spawn_actor(sem_camera, camera_transform, attach_to=self.vehicle,
                                             attachment_type=carla.AttachmentType.SpringArm)
         sem_camera.listen(lambda data: self.sem_camera_callback(data))
         self.actor_list.append(sem_camera)
 
+        # 深度相机
+        depth_camera = self.blueprint_library.find("sensor.camera.depth")
+        depth_camera.set_attribute("image_size_x", f"{self.img_width}")
+        depth_camera.set_attribute("image_size_y", f"{self.img_height}")
+        depth_camera.set_attribute("fov", "110")
+
+        depth_camera = self.world.spawn_actor(depth_camera, camera_transform, attach_to=self.vehicle,
+                                              attachment_type=carla.AttachmentType.SpringArm)
+        depth_camera.listen(lambda data: self.depth_camera_callback(data))
+        self.actor_list.append(depth_camera)
+
+        # RGB相机
+        if self.show_camera:
+            rgb_camera = self.blueprint_library.find("sensor.camera.rgb")
+            rgb_camera.set_attribute("image_size_x", f"{self.img_width}")
+            rgb_camera.set_attribute("image_size_y", f"{self.img_height}")
+            rgb_camera.set_attribute("fov", "110")
+
+            rgb_camera = self.world.spawn_actor(rgb_camera, camera_transform, attach_to=self.vehicle,
+                                                attachment_type=carla.AttachmentType.SpringArm)
+            rgb_camera.listen(lambda data: self.rgb_camera_callback(data))
+            self.actor_list.append(rgb_camera)
+
         """
-        # 获取加速度、角速度
+        雷达传感器
+        """
         # Add IMU sensor to ego vehicle.
-        imu_bp = world.get_blueprint_library().find('sensor.other.imu')
-        imu_location = carla.Location(0, 0, 0)
-        imu_rotation = carla.Rotation(0, 0, 0)
-        imu_transform = carla.Transform(imu_location, imu_rotation)
-        imu_bp.set_attribute("sensor_tick", str(3.0))
-        ego_imu = world.spawn_actor(imu_bp, imu_transform, attach_to=ego_vehicle,
-                                    attachment_type=carla.AttachmentType.Rigid)
-
-        def imu_callback(imu):
-            print("IMU measure:\n" + str(imu) + '\n')
-
-        ego_imu.listen(lambda imu: imu_callback(imu))
-        """
-        """
-        # Add Lane invasion sensor to ego vehicle.
-        lane_bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
-        lane_location = carla.Location(0, 0, 0)
-        lane_rotation = carla.Rotation(0, 0, 0)
-        lane_transform = carla.Transform(lane_location, lane_rotation)
-        ego_lane = world.spawn_actor(lane_bp, lane_transform, attach_to=ego_vehicle,
+        imu_bp = self.blueprint_library.find('sensor.other.imu')
+        # imu_bp.set_attribute("sensor_tick", str(3.0))
+        imu = self.world.spawn_actor(imu_bp, other_transform, attach_to=self.vehicle,
                                      attachment_type=carla.AttachmentType.Rigid)
+        imu.listen(lambda imu: self.imu_callback(imu))
+        self.actor_list.append(imu)
 
-        def lane_callback(lane):
-            print("Lane invasion detected:\n" + str(lane) + '\n')
-
-        ego_lane.listen(lambda lane: lane_callback(lane))
-        """
+        # Add Lane invasion sensor to ego vehicle.
+        lane_bp = self.blueprint_library.find('sensor.other.lane_invasion')
+        lane_invasion = self.world.spawn_actor(lane_bp, other_transform, attach_to=self.vehicle,
+                                               attachment_type=carla.AttachmentType.Rigid)
+        lane_invasion.listen(lambda lane: self.lane_callback(lane))
+        self.actor_list.append(lane_invasion)
 
         # 碰撞检测
         collision_sensor = self.blueprint_library.find("sensor.other.collision")
-        collision_sensor = self.world.spawn_actor(collision_sensor, transform, attach_to=self.vehicle)
-        collision_sensor.listen(lambda event: self.collision_data(event))
+        collision_sensor = self.world.spawn_actor(collision_sensor, vehicle_transform, attach_to=self.vehicle)
+        collision_sensor.listen(lambda event: self.collision_callback(event))
         self.actor_list.append(collision_sensor)
 
-        while self.image_sensor_input is None:
+        while self.sem_camera_input is None or self.depth_camera_input is None:
             time.sleep(0.01)
 
         self.episode_start = time.time()
-        return self.image_sensor_input
+        return (self.sem_camera_input, self.depth_camera_input, self.acceleration, self.angular_velocity)
+        # return self.sem_camera_input
 
     def step(self, action):
         if action == 0:
@@ -213,19 +251,24 @@ class CarEnv:
             else:
                 reward = -20
 
+        if len(self.lane_invasion) != 0:
+            reward -= 20
+
         if self.episode_start + self.run_seconds_per_episode < time.time():
             done = True
 
-        return self.image_sensor_input, reward, done, None
+        return (self.sem_camera_input, self.depth_camera_input, self.acceleration, self.angular_velocity), \
+               reward, done, None
+        # return self.sem_camera_input, reward, done, None
 
 
 class DQNAgent:
     def __init__(self, model, discount_rate=0.99, deque_maxlen=5000, update_frequency=50):
-        self.model = model
         self.discount_rate = discount_rate
         self.update_frequency = update_frequency
 
-        self.target_model = keras.models.clone_model(model)
+        self.model = model
+        self.target_model = keras.models.clone_model(self.model)
         self.target_model.set_weights(self.model.get_weights())
         self.optimizer = Adam(lr=0.01)
         self.loss_fn = keras.losses.Huber()
@@ -238,6 +281,7 @@ class DQNAgent:
             return
 
         batch_experiences = random.sample(self.replay_memory, batch_size)
+        states = batch_experiences[0]
         states, actions, rewards, next_states, dones = [
             np.array([experience[field_index] for experience in batch_experiences])
             for field_index in range(5)]
@@ -261,7 +305,7 @@ class DQNAgent:
             if not soft_update:
                 self.target_model.set_weights(self.model.get_weights())
                 self.target_update_counter = 0
-            else:   # 软更新方式
+            else:  # 软更新方式
                 target_weights = self.target_model.get_weights()
                 online_weights = model.get_weights()
                 for index in range(len(target_weights)):
@@ -274,6 +318,43 @@ class DQNAgent:
         else:
             Q_values = self.model.predict(state[np.newaxis])
             return np.argmax(Q_values[0])
+
+
+def create_model(input_shape, action_num, include_velocity=True):
+    # model = keras.models.Sequential([
+    #     keras.layers.Lambda(lambda image: tf.cast(image, np.float32) / 255, input_shape=(IM_HEIGHT, IM_WIDTH, 3)),
+    #     keras.layers.Conv2D(64, 7, activation="relu", padding="same"),
+    #     keras.layers.AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same'),
+    #     keras.layers.Conv2D(64, 3, activation="relu", padding="same"),
+    #     keras.layers.AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same'),
+    #     keras.layers.Conv2D(64, 3, activation="relu", padding="same"),
+    #     keras.layers.GlobalAvgPool2D(),
+    #     keras.layers.Dense(action_num)
+    # ])
+
+    input_1 = keras.layers.Input(shape=input_shape, name="sem_camera_input")
+    input_2 = keras.layers.Input(shape=input_shape, name="depth_camera_input")
+    input_3 = keras.layers.Input(shape=[3], name="acceleration_input")
+    input_4 = keras.layers.Input(shape=[3], name="angular_velocity_input")
+
+    x = keras.layers.concatenate([input_1, input_2])
+    x = keras.layers.Lambda(lambda image: tf.cast(image, np.float32) / 255)(x)
+
+    x = keras.layers.Conv2D(64, 7, activation="relu", padding="same")(x)
+    x = keras.layers.AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same')(x)
+    x = keras.layers.Conv2D(64, 3, activation="relu", padding="same")(x)
+    x = keras.layers.AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same')(x)
+    x = keras.layers.Conv2D(64, 3, activation="relu", padding="same")(x)
+    x = keras.layers.GlobalAvgPool2D()(x)
+
+    if include_velocity:
+        x = keras.layers.concatenate([x, input_3, input_4])
+        x = keras.layers.Dense(20, activation="relu")(x)
+
+    output = keras.layers.Dense(action_num)(x)
+
+    model = keras.Model(inputs=[input_1, input_2, input_3, input_4], outputs=[output])
+    return model
 
 
 if __name__ == "__main__":
@@ -291,26 +372,9 @@ if __name__ == "__main__":
     action_num = 4
     batch_size = 4  # ??
 
-    # base_model = Xception(weights=None, include_top=False, input_shape=(IM_HEIGHT, IM_WIDTH, 3))
-    # x = base_model.output
-    # x = keras.layers.GlobalAvgPool2D()(x)
-    # output = keras.layers.Dense(action_num)(x)
-    # model = keras.Model(inputs=[base_model.input], outputs=[output])
-
-    model = keras.models.Sequential([
-        keras.layers.Lambda(lambda image: tf.cast(image, np.float32) / 255),
-        keras.layers.Conv2D(64, 7, activation="relu", padding="same", input_shape=(IM_HEIGHT, IM_WIDTH, 3)),
-        keras.layers.AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same'),
-        keras.layers.Conv2D(64, 3, activation="relu", padding="same"),
-        keras.layers.AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same'),
-        keras.layers.Conv2D(64, 3, activation="relu", padding="same"),
-        # keras.layers.AveragePooling2D(pool_size=(5, 5), strides=(3, 3), padding='same'),
-        keras.layers.GlobalAvgPool2D(),
-        # keras.layers.Flatten(),
-        keras.layers.Dense(action_num)
-    ])
+    model = create_model(input_shape=(IM_HEIGHT, IM_WIDTH, 3), action_num=action_num)
     # print(model.summary())
-    model.load_weights(f'models/{MODEL_NAME}.h5')
+    # model.load_weights(f'models/{MODEL_NAME}.h5')
 
     agent = DQNAgent(model, discount_rate=0.99, deque_maxlen=5000)
     env = CarEnv(IM_HEIGHT, IM_WIDTH, show_camera=False, run_seconds_per_episode=20)
@@ -326,7 +390,7 @@ if __name__ == "__main__":
 
         while True:
             epsilon = max(1 - episode / 500, 0.05)
-            action = agent.epsilon_greedy_policy(state / 255, epsilon)
+            action = agent.epsilon_greedy_policy(state, epsilon)
             new_state, reward, done, _ = env.step(action)
             agent.replay_memory.append((state, action, reward, new_state, done))
             state = new_state
@@ -334,9 +398,6 @@ if __name__ == "__main__":
 
             if done:
                 break
-
-        for actor in env.actor_list:
-            actor.destroy()
 
         total_rewards_list.append(episode_reward)
         if episode % 10 == 0:
