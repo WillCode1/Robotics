97a702f39b13e175fc3727cf985fcf968cadf31e
Will <2623556566@qq.com>
2020-07-04 01:17:48 +0800

    asd

--- open code/My-Deep-RL-Keras/other/DQN/Prioritized_DDQN.py
+++ open code/My-Deep-RL-Keras/other/DQN/Prioritized_DDQN.py
@@ -1,8 +1,61 @@
+import heapq
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import gym
-from collections import deque
+
+
+class PrioritizedExperienceQueue:
+    """ Prioritized replay memory using binary heap """
+
+    def __init__(self, maxlen=10000):
+        self.maxlen = maxlen
+        self.memory = []
+
+    def size(self):
+        return len(self.memory)
+
+    def append(self, experience, TDerror):
+        heapq.heappush(self.memory, (experience, -TDerror))
+        if self.size() > self.maxlen:
+            self.memory = self.memory[:-1]
+        heapq.heapify(self.memory)
+
+    def batch(self, batch_size):
+        batch = heapq.nsmallest(batch_size, self.memory)
+        batch = [e for (_, e) in batch]
+        self.memory = self.memory[batch_size:]
+        return batch
+
+    def learn_single(self, state, action, reward, next_state, done):
+        next_Q_value = model.predict(next_state[np.newaxis])[0]
+        best_next_action = np.argmax(next_Q_value, axis=1)
+        next_mask = tf.one_hot(best_next_action, n_outputs).numpy()
+        next_best_Q_value = (target.predict(next_state) * next_mask).sum(axis=1)
+        target_Q_value = (reward + (1 - done) * discount_rate * next_best_Q_value).reshape(-1, 1)
+        # predict Q-values for starting state using the main network
+        y, _ = self.predict(S)
+        y_old = np.array(y)
+
+        # predict best action in ending state using the main network
+        _, A_p = self.predict(S_p)
+
+        # predict Q-values for ending state using the target network
+        y_target, _ = self.predict(S_p, target_network=True)
+
+        # update Q[S, A]
+        if terminal:
+            y[A] = R
+        else:
+            y[A] = R + self.gamma * y_target[A_p]
+
+        return np.abs(y_old[A] - y[A])
+
+    def compute_TDerror(self, experience):
+        state, action, reward, next_state, done = experience
+        TDerror = self.learn_single(state, action, reward, next_state, done)
+        return TDerror
+
 
 # 平衡车问题
 env = gym.make("CartPole-v0")
@@ -13,7 +66,7 @@ keras.backend.clear_session()
 tf.random.set_seed(42)
 np.random.seed(42)
 env.seed(42)
-replay_memory = deque(maxlen=2000)
+replay_memory = PrioritizedExperienceQueue(maxlen=2000)
 
 model = keras.models.Sequential([
     keras.layers.Dense(32, activation="elu", input_shape=[4]),
@@ -31,18 +84,14 @@ def epsilon_greedy_policy(state, epsilon=0):
         return np.argmax(Q_values[0])
 
 
-# 每个经验包含五个元素：状态，智能体选择的动作，奖励，下一个状态，一个知识是否结束的布尔值（done）。
-# 需要一个小函数从接力缓存随机采样。返回的是五个NumPy数组，对应五个经验
 def sample_experiences(batch_size):
-    indices = np.random.randint(len(replay_memory), size=batch_size)
-    batch = [replay_memory[index] for index in indices]
+    batch = replay_memory.batch(batch_size)
     states, actions, rewards, next_states, dones = [
         np.array([experience[field_index] for experience in batch])
         for field_index in range(5)]
     return states, actions, rewards, next_states, dones
 
 
-# 使用ε-贪婪策略的单次玩游戏函数，然后将结果经验存储在replay_buffer中
 def play_one_step(env, state, epsilon):
     action = epsilon_greedy_policy(state, epsilon)
     next_state, reward, done, info = env.step(action)
@@ -61,8 +110,8 @@ loss_fn = keras.losses.Huber()
 
 
 def training_step(batch_size):
-    experiences = sample_experiences(batch_size)
-    states, actions, rewards, next_states, dones = experiences
+    batch_experiences = sample_experiences(batch_size)
+    states, actions, rewards, next_states, dones = batch_experiences
     next_Q_values = model.predict(next_states)
     best_next_actions = np.argmax(next_Q_values, axis=1)
     next_mask = tf.one_hot(best_next_actions, n_outputs).numpy()
@@ -73,7 +122,6 @@ def training_step(batch_size):
         all_Q_values = model(states)
         Q_values = tf.reduce_sum(all_Q_values * mask, axis=1, keepdims=True)
         loss = tf.reduce_mean(loss_fn(target_Q_values, Q_values))
-        print(target_Q_values - Q_values)
     grads = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
