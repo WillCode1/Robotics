7b5e43c5f3a68b28e956189c92d08824eabf0764
Will <2623556566@qq.com>
2020-07-31 20:17:21 +0800

    asd

---
+++ 强化学习/RL/other/Env/env.py
@@ -0,0 +1,15 @@
+import gym
+
+"CartPole-v1"
+"MountainCar-v0"
+'Acrobot-v1'
+'Blackjack-v0'
+# ==========================
+'Pendulum-v0'
+"MountainCarContinuous-v0"
+# ==========================
+"Breakout-v0"
+# ==========================
+
+env_name = 'Breakout-v0'
+env = gym.make(env_name)
