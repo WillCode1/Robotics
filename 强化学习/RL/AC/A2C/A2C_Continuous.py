34bb159fb54b2cd0584c0fe8c1c465abb829d64e
Will <2623556566@qq.com>
2020-07-31 19:38:42 +0800

    asd

--- 强化学习/RL/AC/A2C/A2C_Continuous.py
+++ 强化学习/RL/AC/A2C/A2C_Continuous.py
@@ -1,6 +1,6 @@
-import wandb
+# import wandb
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.layers import Input, Dense, Lambda
 
 import gym
 import argparse
@@ -8,7 +8,7 @@ import numpy as np
 
 tf.keras.backend.set_floatx('float64')
 
-wandb.init(name='A2C', project="deep-rl-tf2")
+# wandb.init(name='A2C', project="deep-rl-tf2")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gamma', type=float, default=0.99)
@@ -20,33 +20,45 @@ args = parser.parse_args()
 
 
 class Actor:
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, action_dim, action_bound, std_bound):
         self.state_dim = state_dim
         self.action_dim = action_dim
+        self.action_bound = action_bound
+        self.std_bound = std_bound
         self.model = self.create_model()
         self.opt = tf.keras.optimizers.Adam(args.actor_lr)
 
     def create_model(self):
-        return tf.keras.Sequential([
-            Input((self.state_dim,)),
-            Dense(32, activation='relu'),
-            Dense(16, activation='relu'),
-            Dense(self.action_dim, activation='softmax')
-        ])
+        state_input = Input((self.state_dim,))
+        dense_1 = Dense(32, activation='relu')(state_input)
+        dense_2 = Dense(32, activation='relu')(dense_1)
+        out_mu = Dense(self.action_dim, activation='tanh')(dense_2)
+        mu_output = Lambda(lambda x: x * self.action_bound)(out_mu)
+        std_output = Dense(self.action_dim, activation='softplus')(dense_2)
+        return tf.keras.models.Model(state_input, [mu_output, std_output])
 
-    def compute_loss(self, actions, logits, advantages):
-        ce_loss = tf.keras.losses.SparseCategoricalCrossentropy(
-            from_logits=True)
-        actions = tf.cast(actions, tf.int32)
-        policy_loss = ce_loss(
-            actions, logits, sample_weight=tf.stop_gradient(advantages))
-        return policy_loss
+    def get_action(self, state):
+        state = np.reshape(state, [1, self.state_dim])
+        mu, std = self.model.predict(state)
+        mu, std = mu[0], std[0]
+        return np.random.normal(mu, std, size=self.action_dim)
+
+    def log_pdf(self, mu, std, action):
+        std = tf.clip_by_value(std, self.std_bound[0], self.std_bound[1])
+        var = std ** 2
+        log_policy_pdf = -0.5 * (action - mu) ** 2 / \
+            var - 0.5 * tf.math.log(var * 2 * np.pi)
+        return tf.reduce_sum(log_policy_pdf, 1, keepdims=True)
+
+    def compute_loss(self, mu, std, actions, advantages):
+        log_policy_pdf = self.log_pdf(mu, std, actions)
+        loss_policy = log_policy_pdf * advantages
+        return tf.reduce_sum(-loss_policy)
 
     def train(self, states, actions, advantages):
         with tf.GradientTape() as tape:
-            logits = self.model(states, training=True)
-            loss = self.compute_loss(
-                actions, logits, advantages)
+            mu, std = self.model(states, training=True)
+            loss = self.compute_loss(mu, std, actions, advantages)
         grads = tape.gradient(loss, self.model.trainable_variables)
         self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
         return loss
@@ -62,7 +74,7 @@ class Critic:
         return tf.keras.Sequential([
             Input((self.state_dim,)),
             Dense(32, activation='relu'),
-            Dense(16, activation='relu'),
+            Dense(32, activation='relu'),
             Dense(16, activation='relu'),
             Dense(1, activation='linear')
         ])
@@ -85,8 +97,12 @@ class Agent:
     def __init__(self, env):
         self.env = env
         self.state_dim = self.env.observation_space.shape[0]
-        self.action_dim = self.env.action_space.n
-        self.actor = Actor(self.state_dim, self.action_dim)
+        self.action_dim = self.env.action_space.shape[0]
+        self.action_bound = self.env.action_space.high[0]
+        self.std_bound = [1e-2, 1.0]
+        
+        self.actor = Actor(self.state_dim, self.action_dim,
+                           self.action_bound, self.std_bound)
         self.critic = Critic(self.state_dim)
 
     def td_target(self, reward, next_state, done):
@@ -117,18 +133,17 @@ class Agent:
 
             while not done:
                 # self.env.render()
-                probs = self.actor.model.predict(
-                    np.reshape(state, [1, self.state_dim]))
-                action = np.random.choice(self.action_dim, p=probs[0])
+                action = self.actor.get_action(state)
+                action = np.clip(action, -self.action_bound, self.action_bound)
 
                 next_state, reward, done, _ = self.env.step(action)
 
                 state = np.reshape(state, [1, self.state_dim])
-                action = np.reshape(action, [1, 1])
+                action = np.reshape(action, [1, self.action_dim])
                 next_state = np.reshape(next_state, [1, self.state_dim])
                 reward = np.reshape(reward, [1, 1])
 
-                td_target = self.td_target(reward * 0.01, next_state, done)
+                td_target = self.td_target((reward+8)/8, next_state, done)
                 advantage = self.advatnage(
                     td_target, self.critic.model.predict(state))
 
@@ -155,11 +170,11 @@ class Agent:
                 state = next_state[0]
 
             print('EP{} EpisodeReward={}'.format(ep, episode_reward))
-            wandb.log({'Reward': episode_reward})
+            # wandb.log({'Reward': episode_reward})
 
 
 def main():
-    env_name = 'CartPole-v1'
+    env_name = 'Pendulum-v0'
     env = gym.make(env_name)
     agent = Agent(env)
     agent.train()
