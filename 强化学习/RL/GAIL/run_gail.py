acee831f52613780f4c3bc080d1e088f524ed629
Will <2623556566@qq.com>
2020-08-02 15:42:14 +0800

    asd

--- 强化学习/RL/GAIL/run_gail.py
+++ 强化学习/RL/GAIL/run_gail.py
@@ -7,27 +7,12 @@ from GAIL.network_models.PPO_Discrete import Agent
 from GAIL.network_models.discriminator import Discriminator
 
 
-def argparser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--logdir', help='log directory', default='log/train/gail')
-    parser.add_argument('--savedir', help='save directory', default='models/gail')
-    parser.add_argument('--iteration', default=int(1e4))
-    parser.add_argument('--gamma', type=float, default=0.99)
-    parser.add_argument('--update_interval', type=int, default=5)
-    parser.add_argument('--actor_lr', type=float, default=0.0005)
-    parser.add_argument('--critic_lr', type=float, default=0.001)
-    parser.add_argument('--clip_ratio', type=float, default=0.1)
-    parser.add_argument('--lmbda', type=float, default=0.95)
-    parser.add_argument('--epochs', type=int, default=3)
-    return parser.parse_args()
-
-
 def main(args):
     env = gym.make('CartPole-v1')
     env.seed(42)
 
     agent = Agent(env, args)
-    discriminator = Discriminator(env, lr=0.1)
+    discriminator = Discriminator(env, lr=args.discriminator_lr)
 
     # 得到专家的观测和行动
     expert_observations = np.genfromtxt('trajectory/observations.csv')
@@ -85,37 +70,35 @@ def main(args):
         act = np.concatenate([expert_act, actions])
         label = np.array([1.] * len(observations) + [0.] * len(observations))
 
-        time = 0
-        while True:
-            time += 1
+        for i in range(10):
             loss = discriminator.model.train_on_batch([obser, act], label)
             # print('loss:', loss)
-            if loss < 1 or time > 30:
-                time = 0
-                break
 
         d_rewards = discriminator.get_rewards(states=observations, actions=actions)
         gaes, td_targets = agent.gae_target(d_rewards, v_preds, v_preds_next, done)
 
         # train policy
-        while True:
-            time += 1
+        for epoch in range(10):
             actor_loss = agent.actor.train(observations, actions, gaes)
             # print('actor_loss:', actor_loss)
-            if actor_loss < 3 or time > 30:
-                time = 0
-                break
-        while True:
-            time += 1
             critic_loss = agent.critic.model.train_on_batch(observations, td_targets)
             # print('critic_loss', critic_loss/len(observations))
-            if critic_loss/len(observations) < 3 or time > 30:
-                time = 0
-                break
 
 
 if __name__ == '__main__':
     tf.keras.backend.set_floatx('float64')
 
-    args = argparser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--savedir', help='save directory', default='models/gail')
+    parser.add_argument('--iteration', default=int(1e4))
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--update_interval', type=int, default=5)
+    parser.add_argument('--actor_lr', type=float, default=0.0005)
+    parser.add_argument('--critic_lr', type=float, default=0.001)
+    parser.add_argument('--discriminator_lr', type=float, default=0.1)
+    parser.add_argument('--clip_ratio', type=float, default=0.1)
+    parser.add_argument('--lmbda', type=float, default=0.95)
+    parser.add_argument('--epochs', type=int, default=3)
+
+    args = parser.parse_args()
     main(args)
